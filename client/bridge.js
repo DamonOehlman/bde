ef998f18ecf5e141d37dd7b2eed58129b496bcb1
Damon Oehlman <damon.oehlman@gmail.com>
2013-10-04 09:01:39 +1000

    Improved error reporting

--- client/bridge.js
+++ client/bridge.js
@@ -1,4 +1,5 @@
 // create an event source back to the bde server
+var crel = require('crel');
 var fs = require('fs');
 var hatch = require('hatch/client');
 var insertCss = require('insert-css');
@@ -6,10 +7,17 @@ var insertCss = require('insert-css');
 // insert some base css
 insertCss(fs.readFileSync(__dirname + '/bridge.css'));
 
+function reportError(err) {
+  var errDiv = crel('div', { class: 'error' },
+    crel('h3', 'Error browserifying'),
+    crel('pre', err.message)
+  );
+
+  document.body.appendChild(errDiv);
+}
+
 hatch(requestId)
   .on('install', function(name) {
-      console.log('need to install: ' + name);
+    console.log('need to install: ' + name);
   })
-  .on('error', function(err) {
-      console.log('caught error while browserifying: ', err);
-  });
+  .on('error', reportError);
